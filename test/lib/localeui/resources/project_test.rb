81bb0f3200aa6ef7f3689a32b13d2ab73feec655
Lars Klindwordt <info@cobarian.de>
2024-11-17 14:37:33 +0100

    add source_file resource

--- test/lib/localeui/resources/project_test.rb
+++ test/lib/localeui/resources/project_test.rb
@@ -10,10 +10,6 @@ module Localeui
 
     ## Test Localeui::Project.init
     test 'Localeui::Project.init should not init the project with unknown api_id' do
-<<<<<<< HEAD
-=======
-      Localeui::User.sign_in('info@cobarian.de', 'kitesurfen')
->>>>>>> 3ecf507 (implement project methods)
       VCR.use_cassette 'test_project_init_with_unknown_api_id' do
         assert_raises Localeui::NotFoundError do
           Localeui::Project.init('pro_12345667890')
@@ -38,10 +34,6 @@ module Localeui
     end
 
     test 'Localeui::Project.info should return project informations' do
-<<<<<<< HEAD
-=======
-      Localeui::User.sign_in('info@cobarian.de', 'kitesurfen')
->>>>>>> 3ecf507 (implement project methods)
       Localeui.config.project_id = 'pro_b220f2fa5637fa4c6c74'
       VCR.use_cassette 'test_project_info_with_valid_api_id' do
         response = Localeui::Project.info
